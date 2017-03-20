d3326ae8baa6727e01a3c7c6054cde3249c78628
Jaya Wijono <jayzzwijono@yahoo.com>
2017-03-20 23:48:52 +1100

    User React Router v4 Link Component for Navigation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+import './App.css';
 
 const Home = () => <h1>Home</h1>
+
+const Links = () => (
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to={{pathname: '/about'}}>About</Link>
+    <Link to="/about-2">About-2</Link>
+    <Link to="/contact">Contact</Link>
+  </nav>
+)
 
 const App = () => (
   <Router>
     <div>
+      <Links />
       <Route exact path="/" component={Home} />
       <Route path="/about" render={() => <h1>About</h1>} />
       <Route
         path="/about-2"
         children={({match}) => match && <h1>About with children</h1>} />
+      <Route path="/contact" render={() => <h1>Contact</h1>} />
     </div>
   </Router>
 )
